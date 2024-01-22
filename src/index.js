72a5ec0de20ee9b3c823d3ef26c28918df395079
Fardin7864 <fardinhasan1888@gmail.com>
2024-01-22 13:08:55 +0600

    use express , create utils of error , response and asynch handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+const port = process.env.PORT || 8000
 
 dotenv.config({
     path: '../.env'
 })
 
-connectDB();
+connectDB()
+.then(() => { 
+    app.listen(port, () => { 
+        console.log(`👏 Server is runnig at port : ${port}`)
+     })
+ })
+.catch((error) => { 
+    console.log("MonngoDB connection error!")
+ })
