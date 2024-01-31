9ab1c7fdc428fa76f551395181fee1f85bd89a05
Fardin7864 <fardinhasan1888@gmail.com>
2024-01-31 20:31:44 +0600

    add multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloydinary = async (localFilePath) =>{
+    try {
+        if (!localFilePath) return null;
+       const respons = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: 'auto'
+        })
+        console.log("file is uploaded on cloudinary", respons.url)
+        return respons;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //removed the locally saved temporary file 
+        return null;
+    }
+}
+
+
+export {uploadOnCloydinary}
+
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
